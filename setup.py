90cedfd7edd01bb33cec5b6a91722706603b183c
tore-statsig <74584483+tore-statsig@users.noreply.github.com>
2021-09-15 13:28:40 -0700

    Fix sdk installation for x86/other platforms

--- setup.py
+++ setup.py
@@ -1,15 +1,44 @@
-from os import path
+import os
+from distutils.errors import CompileError
 from setuptools import setup
+from subprocess import call
+from distutils.command.build import build
 
-with open(path.join(path.abspath(path.dirname(__file__)), 'statsig', 'version.py')) as f:
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'statsig', 'version.py')) as f:
     exec(f.read())  # pylint: disable=exec-used
 
-with open(path.join(path.abspath(path.dirname(__file__)), 'README.md')) as r:
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md')) as r:
     README = r.read()
+
+class StatsigBuild(build):
+    def run(self):
+        clean = [
+            'make',
+            '-C',
+            'statsig/shared/',
+            'clean',
+        ]
+
+        make = [
+            'make',
+            '-C',
+            'statsig/shared/',
+            'install',
+        ]
+        def compile():
+            call(clean)
+            out = call(make)
+            if out != 0: 
+                raise CompileError('Failed to build Statsig shared module.  Do you have go installed?')
+
+        self.execute(compile, [], 'Building statsig shared module')
+
+        build.run(self)
 
 setup(
     name='statsig',
     version=__version__, # pylint: disable=undefined-variable
+    cmdclass={'build': StatsigBuild},
     description='Statsig Python Server SDK',
     long_description=README,
     long_description_content_type="text/markdown",
