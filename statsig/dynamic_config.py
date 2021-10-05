d923eb4dd36ca949a3ca5614aba8dcd3137b54fc
tore-statsig <74584483+tore-statsig@users.noreply.github.com>
2021-10-05 15:20:47 -0700

    Add dynamic config getters (#14)
    * Add dynamic config getters
    
    * fallback -> default

--- statsig/dynamic_config.py
+++ statsig/dynamic_config.py
@@ -10,9 +10,29 @@ class DynamicConfig:
             rule = ""
         self.rule_id = rule
     
+    def get(self, key, default=None):
+        """Returns the value of the config at the given key
+        or the provided default if the key is not found
+        """
+        return self.value.get(key, default)
+    
+    def get_typed(self, key, default=None):
+        """Returns the value of the config at the given key
+        iff the type matches the type of the provided default.
+        Otherwise, returns the default value
+        """
+        res = self.value.get(key, default)
+        if default is None:
+            return res
+        if type(default) != type(res):
+            return default
+        return res
+
     def get_value(self):
+        """Returns the underlying value of this DynamicConfig"""
         return self.value
     
     def get_name(self):
+        """Returns the name of this DynamicConfig"""
         return self.name
     
---
+++ tests/test_dynamic_config.py
@@ -0,0 +1,47 @@
+
+import unittest
+
+from statsig.dynamic_config import DynamicConfig
+
+
+class TestStatsig(unittest.TestCase):
+
+    def test_getters(self):
+        config = DynamicConfig({
+            "str": "string",
+            "num": 4,
+            "bool": True,
+            "arr": [17],
+        }, "my_config", "default")
+
+        self.assertEqual(config.get_name(), "my_config")
+        self.assertEqual(config.rule_id, "default")
+
+        self.assertEqual(config.get("str"), "string")
+        self.assertEqual(config.get("num"), 4)
+        self.assertEqual(config.get("bool"), True)
+        self.assertEqual(config.get("arr"), [17])
+        self.assertEqual(config.get("nonexistent"), None)
+
+        self.assertEqual(config.get("str", "fallback"), "string")
+        self.assertEqual(config.get("str", 33), "string")
+        self.assertEqual(config.get("num", 7), 4)
+        self.assertEqual(config.get("num", "str"), 4)
+        self.assertEqual(config.get("bool", False), True)
+        self.assertEqual(config.get("bool", "hello"), True)
+        self.assertEqual(config.get("arr", ["test"]), [17])
+        self.assertEqual(config.get("nonexistent", 42), 42)
+        self.assertEqual(config.get("nonexistent", "hi"), "hi")
+        
+        self.assertEqual(config.get_typed("str", 17), 17)
+        self.assertEqual(config.get_typed("num", "default"), "default")
+        self.assertEqual(config.get_typed("bool", ["test"]), ["test"])
+        self.assertEqual(config.get_typed("arr", 33), 33)
+        self.assertEqual(config.get_typed("nonexistent", "hello"), "hello")
+
+        ## List types do not differentiate the type of the values in the list
+        self.assertEqual(config.get_typed("arr", ["str_arr"]), [17])
+        
+
+if __name__ == '__main__':
+    unittest.main()
