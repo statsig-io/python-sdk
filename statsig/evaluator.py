100092133e796b5d24f5ed39945e5d009ae2a5af
Weihao Ding <158090588+weihao-statsig@users.noreply.github.com>
2025-02-27 15:38:06 -0800

    fix: id_type to idType (#408)
    as title

--- statsig/evaluator.py
+++ statsig/evaluator.py
@@ -277,7 +277,7 @@ class _Evaluator:
 
     def __finalize_eval_result(self, config, end_result, did_pass, rule, is_nested=False):
         end_result.boolean_value = did_pass
-        end_result.id_type = config.get("id_type", "")
+        end_result.id_type = config.get("idType", "")
         if config.get("forwardAllExposures", False):
             end_result.forward_all_exposures = True
 
